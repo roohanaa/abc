2bcfc9925573dd11f900c944ebd1e3e91f160829
roohanaa <94599470+roohanaa@users.noreply.github.com>
2022-02-04 05:45:42 -0800

    jolly jumper

---
+++ jollyjumper-main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jollyjumper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 5,1,4,2,-1,6 };
+            int n = a.Length;
+            jollyjumper jj = new jollyjumper();
+           string s= jj.isJolly(a,n);
+            Console.WriteLine(s);
+          
+        }
+    }
+}
---
+++ jollyjumper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jollyjumper
+{
+    internal class jollyjumper
+    {
+       
+		public String isJolly(int[] array, int number)
+		{
+			
+		
+			int[] decendarray=new int[number];
+			
+			for (int i = 0; i < number - 1; i++)
+			{
+				
+				int d = Math.Abs(array[i] - array[i + 1]);
+
+				decendarray[i] = d;
+				
+			}
+			for (int i = 0; i < decendarray.Length - 1; i++)
+			{
+				if ((decendarray[i] < decendarray[i + 1]))
+				{
+					return "not jolly";
+				}
+			}
+			return "jolly";
+		}
+
+	
+		
+	
+
+	
+	}
+}
