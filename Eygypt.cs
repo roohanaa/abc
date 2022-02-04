397d436ed30b9ce33613287bf31c9ce53398ed03
roohanaa <94599470+roohanaa@users.noreply.github.com>
2022-02-04 05:46:42 -0800

    eygypt

---
+++ Eygypt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS1
+{
+    internal class Eygypt
+    {
+        int guess1,guess2,guess3;
+
+        public Eygypt(int guess1, int guess2, int guess3)
+        {
+            this.guess1 = guess1;
+            this.guess2 = guess2;
+            this.guess3 = guess3;
+        }
+
+        public string perfectsquare()
+        {
+            int temphypotneus = (guess1 * guess1)+(guess2 * guess2)  ;
+            if (temphypotneus == 0)
+            {
+                return null;
+            }
+            if ((guess3 * guess3) == temphypotneus)
+            {
+                return "right";
+            }
+            
+            else
+            {
+                return "wrong";
+            }
+        }
+    }
+}
---
+++ eygypt-main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int g1, g2, g3;
+
+            string fileName = @"C:\Users\roohana.siddiqui\source\repos\PS1\PS1\abc.txt";
+
+            string[] lines = File.ReadAllLines(fileName);
+            Console.WriteLine("Enter 3 numbers");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string var = lines[i];
+                string[] var1 = var.Split(' ');
+                g1 = int.Parse(var1[0]);
+                g2=int.Parse(var1[1]);
+                g3=int.Parse(var1[2]);
+                Eygypt e = new Eygypt(g1, g2, g3);
+                String answer = e.perfectsquare();
+                Console.WriteLine(answer);
+            }
+                
+                
+            
+           
+         
+            
+        }
+    }
+}
